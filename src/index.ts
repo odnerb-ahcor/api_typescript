6ce0d00d64d9b331414f9109d7cdcb743003cbcc
Brendo Rocha <brendorocha1420@gmail.com>
2024-09-24 19:37:48 -0300

    add doc api swagger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
-// import dotenv from "dotenv";
+import swaggerUi from "swagger-ui-express";
+
 import router from "./router/routes";
+import swaggerDocs from "./swagger.json";
 import { routerProtected } from "./router/routerProtected";
 import { errorMiddleware } from './middlewares/error';
 
@@ -11,6 +13,7 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(router);
 app.use(routerProtected);
 app.use(errorMiddleware);
